d9b2a5c43eee40072d5d47a927647d998a4c31ff
said <said.oubari184@gmail.com>
2022-12-04 14:04:21 +0100

    initialisation du projet mongo db spring crud

---
+++ springbootmongodb/src/test/java/intsaid/springbootmongodb/SpringbootmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package intsaid.springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
